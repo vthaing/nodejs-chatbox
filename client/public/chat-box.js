67ce74c38fa7bd3eee651bd38c9860928fea0a90
vthaing <vthaing@gmail.com>
2024-03-25 09:57:58 +0700

    Trying to init data

--- client/public/chat-box.js
+++ client/public/chat-box.js
@@ -84,7 +84,7 @@ function chatBoxesManagement () {
     }
 
     this.getApiInitChatBoxEndpoint = function () {
-        return 'http://localhost:3001/brand-chat/init-chat';
+        return 'http://localhost:3001/api/brand-chat/init-chat';
     }
 
 }
