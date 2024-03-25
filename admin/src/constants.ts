568ce78883c11466d7909cd1f5c84504b0a6a1c2
vthaing <vthaing@gmail.com>
2024-03-25 09:58:01 +0700

    Update the environment variable
    (cherry picked from commit 291f9771b94b6396e321a7f88f177bd102c5c786)

--- admin/src/constants.ts
+++ admin/src/constants.ts
@@ -1,5 +1,4 @@
 export const TOKEN_KEY = "admin-auth-token";
 export const AUTH_USER_DATA_KEY = "admin-auth-user-data";
 export const API_URL = process.env.REACT_APP_API_URL ?? 'http://localhost:3001/api';
-
-export const CHATBOX_LIBRARY_URL = process.env.CHATBOX_LIBRARY_URL ?? 'http://localhost:3001/api/client-scripts/chat-box.js';
+export const CHATBOX_LIBRARY_URL = process.env.REACT_APP_CHATBOX_LIBRARY_URL ?? 'http://localhost:3001/api/client-scripts/chat-box.js';
