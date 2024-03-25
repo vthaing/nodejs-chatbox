e460f78bb23f44f57b2b8732e63eb56c8b458b6a
vthaing <vthaing@gmail.com>
2024-03-25 09:58:02 +0700

    working with react lightbox
    (cherry picked from commit 086bd30a8af4126d6510ecb5d1b4141942f17e16)

--- client/src/components/MessageMediaItems.tsx
+++ client/src/components/MessageMediaItems.tsx
@@ -1,24 +1,47 @@
-import React, {useEffect, useState} from 'react'
+import React, { useState} from 'react'
 import {IMediaItem} from "../context/chat/ChatContext";
-import { Image } from 'antd';
+import Lightbox from "yet-another-react-lightbox";
 
 export type MessageMediaItemsProps = {
     mediaItems: IMediaItem[]
 }
 export const MessageMediaItems: React.FC<MessageMediaItemsProps> = ({mediaItems}) => {
 
+    const [lightboxIndex, setLightboxIndex] = useState(-1);
+
+
+
     return (
         <div className={'message-media-items'}>
-            <Image.PreviewGroup>
-                {mediaItems.map((mediaItem) => (
-                    <Image className={'message-media-item'} src={process.env.REACT_APP_API_URL + mediaItem.url}
-                       style={{
+            {mediaItems.map((mediaItem, mediaIndex) => (
+                <button
+                    onClick={(e) => {
+                        e.preventDefault();
+                        setLightboxIndex(mediaIndex)
+                    }}
+                    key={'media-item-link-' + mediaItem.id}
+                >
+                    <img
+                        alt={mediaItem.name}
+                        key={'media-item-img-' + mediaItem.id} className={'message-media-item'} src={process.env.REACT_APP_API_URL + mediaItem.url}
+                        style={{
                            maxHeight: 100, maxWidth: 100, margin: 2, padding: 3,
                            border: "1px solid #d9d9d9", borderRadius: 5
                         }}
                     />
+                </button>
                 ))}
-            </Image.PreviewGroup>
+            <Lightbox
+                open={lightboxIndex > 0}
+                index={lightboxIndex}
+                close={() =>setLightboxIndex(-1)}
+                slides={mediaItems.map((mediaItem) => ({
+                    src: mediaItem.url,
+                    key: mediaItem.id,
+                    width: mediaItem.imageWidth ?? 100,
+                    height: mediaItem.imageHeight ?? 100
+                }))}
+            />
         </div>
     )
 }
