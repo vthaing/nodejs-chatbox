cd39e2b7f0a84c30e64d05be8d1890063a40f14e
vthaing <vthaing@gmail.com>
2024-03-25 09:57:59 +0700

    Working with the fucking form....

--- admin/src/pages/users/button-ban-user.tsx
+++ admin/src/pages/users/button-ban-user.tsx
@@ -3,7 +3,7 @@ import {IUser} from "../../interfaces";
 import {Button, Popconfirm} from "antd";
 import {CheckOutlined, StopOutlined} from "@ant-design/icons";
 import {useApiUrl, useCustom, useCustomMutation, useUpdate} from "@pankod/refine-core";
-import {Modal, useModal} from "@pankod/refine-antd";
+import {Form, Input, Modal, useForm, useModal} from "@pankod/refine-antd";
 
 type ButtonBanUserProps = {
     record: IUser
@@ -12,9 +12,29 @@ type ButtonBanUserProps = {
 
 export const ButtonBanUser: React.FC<ButtonBanUserProps> = ({record, onSuccess}) => {
 
-    const { modalProps, show } = useModal();
-    const handleFormSubmit = () => {
+    const [form] = Form.useForm();
+    const [formValues, setFormValues] = useState({
+        reason: '',
+        duration: null
+    });
 
+
+    const { modalProps, show } = useModal();
+    const handleFormSubmit = (values: any) => {
+        alert('sdsdsd');
+    }
+
+    const handleOnChange = (
+        e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    ) => {
+        setFormValues({
+            ...formValues,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const onOk = () => {
+        form.submit();
     }
 
     return (
@@ -26,8 +46,17 @@ export const ButtonBanUser: React.FC<ButtonBanUserProps> = ({record, onSuccess})
                 onClick={show}
             >
             </Button>
-            <Modal {...modalProps} onOk={handleFormSubmit}>
-                Hello moto
+            <Modal {...modalProps} onOk={() => onOk()}
+               title={`Ban user ${record.displayName}`}
+            >
+                <Form form={form} onFinish={handleFormSubmit}>
+                    <Form.Item label="Reason" name="reason">
+                        <Input onChange={handleOnChange} />
+                    </Form.Item>
+                    <Form.Item label="Duration" name="duration">
+                        <Input onChange={handleOnChange} type={'number'} />
+                    </Form.Item>
+                </Form>
             </Modal>
         </>
     );
