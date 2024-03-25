351815524b627d0a0f9dacfc5384fb81553b337f
vthaing <vthaing@gmail.com>
2024-03-25 09:58:00 +0700

    Fix bug the paging service doesn't expose total count
    (cherry picked from commit 636fe70fdc3b332ddb7a7c446328b1c69dc68f06)

--- server/src/modules/common/service/paging.service.ts
+++ server/src/modules/common/service/paging.service.ts
@@ -5,7 +5,16 @@ import { Response } from 'express';
 @Injectable()
 export class PagingService {
   createPaginationResponse(res: Response, paginateResult: PaginateResult<any>) {
-    res.setHeader('x-total-count', paginateResult.totalDocs.toString());
+    const existedExposedHeaders =
+      res.getHeader('Access-Control-Expose-Headers') ?? '';
+
+    res.setHeader(
+      'Access-Control-Expose-Headers',
+      (existedExposedHeaders ? existedExposedHeaders + ', ' : '') +
+        ' x-total-count',
+    );
+    
+    res.setHeader('x-total-count', paginateResult.totalDocs);
     return res.send(paginateResult.docs);
   }
 }
