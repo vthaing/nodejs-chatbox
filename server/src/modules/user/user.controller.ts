9929a8295aa211982c8178a289162f293787adbb
vthaing <vthaing@gmail.com>
2024-03-25 09:57:59 +0700

    Fix bug user detail page was wrong

--- server/src/modules/user/user.controller.ts
+++ server/src/modules/user/user.controller.ts
@@ -33,7 +33,7 @@ export class UserController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.userService.findOne({ id });
+    return this.userService.findById(id);
   }
 
   @Get('me')
