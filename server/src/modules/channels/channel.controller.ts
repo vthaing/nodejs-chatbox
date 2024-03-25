df4f8b20d6223e7bf34d26e944e4d46f67b01f12
vthaing <vthaing@gmail.com>
2024-03-25 09:57:55 +0700

    API to get user channels

--- server/src/modules/channels/channel.controller.ts
+++ server/src/modules/channels/channel.controller.ts
@@ -19,33 +19,36 @@ import { JwtAuthGuard } from '../auth/guards/jwt.guard';
 @UseGuards(JwtAuthGuard)
 @Controller('channels')
 export class ChannelController {
-  constructor(private readonly channelsService: ChannelService) {}
+  constructor(private readonly channelService: ChannelService) {}
 
   @Post()
   create(@Body() createChannelDto: CreateChannelDto) {
-    return this.channelsService.create(createChannelDto);
+    return this.channelService.create(createChannelDto);
   }
 
   @Get()
   findAll() {
-    return this.channelsService.findAll();
+    return this.channelService.findAll();
+  }
+
+  //@TODO: should move this route to the user module. The URL should be: user/:userId/channels
+  @Get('user-channels/:userId')
+  findAllUserChannels(@Param('userId') userId: string) {
+    return this.channelService.findAll({ members: { $all: [userId] } });
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.channelsService.findOne(id);
+    return this.channelService.findOne(id);
   }
 
   @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateChannelDto: UpdateChannelDto,
-  ) {
-    return this.channelsService.update(id, updateChannelDto);
+  update(@Param('id') id: string, @Body() updateChannelDto: UpdateChannelDto) {
+    return this.channelService.update(id, updateChannelDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.channelsService.remove(id);
+    return this.channelService.remove(id);
   }
 }
