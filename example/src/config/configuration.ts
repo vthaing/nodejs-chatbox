670f926d7305ff76e89355da3e510d1dd9f7b246
vthaing <vthaing@gmail.com>
2024-03-25 09:57:59 +0700

    Add client settings

--- example/src/config/configuration.ts
+++ example/src/config/configuration.ts
@@ -3,5 +3,6 @@ import * as process from 'process';
 export default () => ({
   port: parseInt(process.env.PORT, 10) || 3005,
   brandId: process.env.BRAND_ID,
+  chatBoxSecretKey: process.env.CHATBOX_SECRET_KEY,
   chatBoxLibraryUrl: process.env.CHATBOX_LIBRARY_URL,
 });
